eef54c46b7380895c2c67b1525d89a6d450509d8
Jordan Ball <cjordanball@ballwebdev.com>
2023-05-25 14:11:01 -0400

    add education content

---
+++ src/content/education.ts
@@ -0,0 +1,89 @@
+export const education = {
+	formal: {
+		headlineMain: 'Education',
+		headlineSub: 'Degree Programs',
+		schools: [
+			{
+				name: 'Manhattan School of Music',
+				degree: 'Bachelor of Music - Classical Guitar',
+				location: 'New York City',
+			},
+			{
+				name: 'Georgetown University Law Center',
+				degree: 'Juris Doctor',
+				location: 'New York City',
+			},
+			{
+				name: 'New York University School of Law',
+				degree: 'LL.M - Taxation',
+				location: 'New York City',
+			},
+		],
+	},
+	other: {
+		headlineMain: 'Education',
+		headlineSub: 'Other',
+		courses: [
+			{
+				provider: 'General Assembly',
+				name: 'Web Development Immersive',
+				startDate: 'November 2014',
+				endDate: 'February 2015',
+				location: 'Seattle, WA',
+				description:
+					'Full-time immersive coding boot camp, building web apps using MEAN stack and Ruby/Rails framework. Devoted approximately 80 to 90+ hours per week, including class sessions, homework, and building full-scale web applications. Graduated with extensive experience in designing and building web apps from front-end design to server-side/database.',
+			},
+			{
+				provider: 'Harvard University/edX',
+				name: 'CS50 - Introduction to Computer Science',
+				startDate: null,
+				endDate: 'May 2015',
+				location: 'Online',
+				description:
+					'<p>Harvard’s gateway computer science course. Although considered one of the most rigorous undergraduate courses at Harvard, it has proved so popular that in 2015 it was adopted by Yale as its introductory computer science course, via lectures streamed from the Harvard campus!</p><p>The first two-thirds of this twelve-week course are implemented in C, followed by sections addressing the Internet, web applications, and computer security, utilizing PHP, JavaScript, SQL, and other common web languages/frameworks. Graded assignments consist of nine problem sets plus a final project. Topics covered include: C syntax and programming structures; debugging techniques, particularly use of the gdb debugging tool; analysis and implementation of algorithms, with a focus on asymptoptic run times of common searching and sorting methods; data structures, including arrays, trees, tries, hash tables, linked lists, queues, and stacks; memory allocation and management, including use of pointers, heap, and stack memory; and file input/output, both dynamically and at the command line.</p>',
+			},
+			{
+				provider: 'Code Fellows',
+				name: 'Foundations II: Javascript',
+				startDate: null,
+				endDate: 'October 2014',
+				location: 'Seattle, WA',
+				description:
+					'Survey of the fundamentals of JavaScript, including basics of functional, asynchronous programming, object-oriented JavaScript concepts, AJAX/JSON, use of libraries such as Lodash, Underscore, jQuery, use of Git/GitHub.',
+			},
+		],
+	},
+	courses: {
+		providers: ['Stanford University', 'Udemy', 'egghead.io', 'Treehouse'],
+		courseNames: [
+			'CS193p - Developing Apps for iOS',
+			'Docker for DevOps',
+			'Docker Technologies for DevOps and Developers',
+			'React Native with Redux',
+			'Advanced React and Redux',
+			'Modern React with Redux',
+			'Build Web Apps with React and Flux',
+			"The Complete Developer's Guide to MongoDB",
+			'Complete Guide to Angular 4',
+			'Learn and Understand Angular',
+			'Unit Testing Angular',
+			"Webpack2: The Complete Developer's Guide",
+			"ES6 JavaScript: The Complete Developer's Guide",
+			'NodeJS Basics',
+			'Learn and Understand Angular',
+			'Build a Simple Dynamic Site with NodeJS',
+			'NodeJS Codeless API Creation: Up and Running with Swagger',
+			'Use D3 to Build Interactive Charts with JavaScript',
+			'Backbone Tutorial',
+			'Git Basics',
+			'HTML / HTML Tables / HTML Forms',
+			'CSS Basics',
+			'CSS - Beyond the Basics',
+			'Ruby Basics',
+			'Java Basics',
+			'AJAX Basics',
+			'NodeJS Basics',
+			'Understanding “this” in JavaScript<',
+		],
+	},
+};
