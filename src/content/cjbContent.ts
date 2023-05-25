e4302aae15995dcd57e2355577ca18588ddbd968
Jordan Ball <cjordanball@ballwebdev.com>
2023-05-25 12:32:01 -0400

    add contact and skills info into text file

---
+++ src/content/cjbContent.ts
@@ -0,0 +1,61 @@
+export const cjbContent = {
+	contactInfo: {
+		fullName: 'C. Jordan Ball III',
+		nickName: 'Jordan',
+		lastName: 'Ball',
+		telephone: '757-425-6200',
+		webpage: 'https://www.ballwebdev.com',
+		github: 'https://github.com/cjordanball',
+		linkedIn: 'www.linkedin.com/in/cjordanball',
+	},
+	headline: {
+		position: 'Senior Software Engineer',
+		highlightBar: ['JavaScript', 'TypeScript', 'NodeJS', 'React'],
+	},
+	profile: {
+		heading: 'Profile',
+		text: 'Former attorney turned programmer/web developer, with experience designing and building full-stack web applications within the NodeJS / Express environment. Highly skilled as a problem solver, <span class="red">ranked 1st or 2nd out of 125,000+ users on the coderbyte.com coding challenge website.</span> A lifelong learner who combines a love of new technologies with a passion for digging deep into programming fundamentals.',
+	},
+	skills: {
+		headline: {
+			mainHeadline: 'Technical Skills',
+			parenthetical: '(most developed are bolded)',
+		},
+		languages: {
+			headline: 'Programming/Markup Languages:',
+			entries: ['JavaScript', 'C', 'Go', 'HTML', 'CSS'],
+		},
+		serverFrameworks: {
+			headline: 'Server-Side Frameworks:',
+			entries: ['NodeJS', 'Express', 'Swagger'],
+		},
+		browserFrameworks: {
+			headline: 'Front-End Frameworks:',
+			entries: ['React/Redux', 'Angular', 'AngularJS', 'Bootstrap'],
+		},
+		data: {
+			headline: 'Database:',
+			entries: ['MongoDB / Mongoose', 'PostgreSQL', 'Sequelize', 'GraphQL'],
+		},
+		testing: {
+			headline: 'Testing',
+			entries: ['Mocha', 'Jasmine'],
+		},
+		misc: {
+			headline: 'Other',
+			entries: [
+				'AWS (EC2, S3, Route53, IAM, etc.)',
+				'Git/GitHub',
+				'Bitbucket',
+				'Heroku',
+				'Webpack',
+				'Docker',
+				'Digital Ocean',
+				'Pivotal Tracker',
+				'JIRA',
+				'agile development',
+				'Oracle Web Content Server',
+			],
+		},
+	},
+};
